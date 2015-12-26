4c9934fb6d6942c1919d25833ce0c30b60ca9347
Tomer Rosenfeld <tomerosenfeld007@gmail.com>
2015-12-27 00:20:56 +0200

    Added option to filter by country

--- directives/header/searchbar.js
+++ directives/header/searchbar.js
@@ -2,44 +2,6 @@
  * Created by tomer on 12/24/15.
  */
 app.controller('SearchController', function ($scope, $rootScope, $http, $filter) {
-    $scope.teams = undefined;
-    for (var i = 0; i < 12; i++) {
-        loadAll(i)
-    }
-
-    function loadAll(index) {
-        $http({
-            method: 'GET',
-            url: 'http://www.thebluealliance.com/api/v2/teams/' + index,
-            headers: {
-                'X-TBA-App-Id': 'greenblitz:scouting-system:v01'
-            }
-        }).then(function successCallback(response) {
-            for (var i = 0; i < response.data.length; i++) {
-
-                if (!$rootScope.isStringEmpty(response.data[i].nickname)) {
-                    response.data[i].name = response.data[i].nickname;
-                }
-                if ($rootScope.isStringEmpty(response.data[i].name)) {
-                    console.log("remove " + i);
-                    response.data.splice(i, 1);
-                }
-            }
-            console.log(response.data);
-            if ($scope.teams == null) {
-                $scope.teams = response.data
-            }
-            else {
-                addArrayTo(response.data, $scope.teams)
-            }
-
-            $rootScope.teams += $scope.teams;
-        }, function errorCallback(response) {
-            return response;
-            // called asynchronously if an error occurs
-            // or server returns response with an error status.
-        });
-    }
 
     $scope.getMatches = function (text) {
         return $filter('filter')($scope.teams, text)
@@ -49,16 +11,11 @@ app.controller('SearchController', function ($scope, $rootScope, $http, $filter)
         return $rootScope.isStringEmpty(item.nickname) ? item.name : item.name
     };
 
-    function addArrayTo(arr1, arr2) {
-        for (var i = 0; i < arr1.length; i++) {
-            arr2.push(arr1[i])
-        }
-    }
-
     $scope.search = function (button) {
         $rootScope.searched = $scope.selectedItem;
         console.log($rootScope.searched);
         $rootScope.changeView(button);
+        $rootScope.toggleSideNav()
     }
 });
 app.directive('searchBar', function () {
--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,4 @@
-app.controller('MainController', function ($scope, $rootScope) {
+app.controller('MainController', function ($scope, $rootScope, $http) {
     $rootScope.buttons = [
         {
             name: "Home",
@@ -28,4 +28,54 @@ app.controller('MainController', function ($scope, $rootScope) {
     $rootScope.isStringEmpty = function (string) {
         return string == null || !string || 0 === string.length
     };
+    $scope.teams = undefined;
+
+    $rootScope.loadAll = function (index) {
+        $http({
+            method: 'GET',
+            url: 'http://www.thebluealliance.com/api/v2/teams/' + index,
+            headers: {
+                'X-TBA-App-Id': 'greenblitz:scouting-system:v01'
+            }
+        }).then(function successCallback(response) {
+            for (var i = 0; i < response.data.length; i++) {
+
+                if (!$rootScope.isStringEmpty(response.data[i].nickname)) {
+                    response.data[i].name = response.data[i].nickname;
+                }
+                if ($rootScope.isStringEmpty(response.data[i].name)) {
+                    console.log("remove " + i);
+                    response.data.splice(i, 1);
+                }
+            }
+            console.log(response.data);
+            if ($scope.teams == null) {
+                $scope.teams = response.data
+            }
+            else {
+                addArrayTo(response.data, $scope.teams)
+            }
+            $rootScope.teams = $scope.teams
+        }, function errorCallback(response) {
+            return response;
+            // called asynchronously if an error occurs
+            // or server returns response with an error status.
+        });
+    };
+    $rootScope.loadData = function () {
+        $rootScope.country = $rootScope.limitSearch ? null : "Israel";
+        $scope.teams = null;
+        $rootScope.teams = null;
+        for (var i = 0; i < 12; i++) {
+            $rootScope.loadAll(i)
+        }
+    };
+    $rootScope.loadData();
+
+    function addArrayTo(arr1, arr2) {
+        for (var i = 0; i < arr1.length; i++) {
+            arr2.push(arr1[i])
+        }
+    }
+
 });
