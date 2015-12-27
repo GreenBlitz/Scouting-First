a7dbe740f66973b87702fccca317b2ba94896b70
Tomer Rosenfeld <tomerosenfeld007@gmail.com>
2015-12-27 18:23:52 +0200

    Added save button in settings & removed annoying alert in search page

--- scripts/controllers/searchPage.js
+++ scripts/controllers/searchPage.js
@@ -4,9 +4,5 @@
 app.controller('SearchPageController', function ($scope, $rootScope) {
     if ($rootScope.searched == null) {
         $rootScope.changeView($rootScope.buttons[0]);
-        setTimeout(function () {
-            alert("You didn't search for anything..");
-        }, 1)
-
     }
 });
